f7208f6389b593d964d2254c15efd36d304032dc
RunAtTekky <varunrawat343@gmail.com>
2026-06-29 13:41:37 +0530

    Add: backend directory

---
+++ Skidadle-backend/src/main/java/com/example/skidadlebackend/SkidadleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.skidadlebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkidadleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkidadleBackendApplication.class, args);
+    }
+
+}
---
+++ Skidadle-backend/src/test/java/com/example/skidadlebackend/SkidadleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skidadlebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkidadleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
