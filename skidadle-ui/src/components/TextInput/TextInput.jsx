f61e7ba5e1bab7bd6c4360bb76aec0316fe28678
suryapratapsingh26 <146753809+suryapratapsingh26@users.noreply.github.com>
2026-07-02 00:32:10 +0530

    Fix TextInput indentation and formatting

--- skidadle-ui/src/components/TextInput/TextInput.jsx
+++ skidadle-ui/src/components/TextInput/TextInput.jsx
@@ -1,20 +1,23 @@
 import "./TextInput.css";
-function TextInput({label, defaultValue, min, max}){
-    return (
-        <div className = "input-group">
-            <label className = "label">{label}</label>
-            <input
-            type = "number"
-            className = "input"
-            defaultValue={defaultValue}
-            min = {min}
-            max = {max}
-            />
-            <p className = "hint">
-                Min {min} - Max {max}
-                
-                 </p>
-        </div>
-    )
+
+function TextInput({ label, defaultValue, min, max }) {
+  return (
+    <div className="input-group">
+      <label className="label">{label}</label>
+
+      <input
+        type="number"
+        className="input"
+        defaultValue={defaultValue}
+        min={min}
+        max={max}
+      />
+
+      <p className="hint">
+        Min {min} - Max {max}
+      </p>
+    </div>
+  );
 }
+
 export default TextInput;
