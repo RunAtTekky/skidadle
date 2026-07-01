8eb600105279d95bc71f5adeac87d003d8e0289b
RunAtTekky <varunrawat343@gmail.com>
2026-07-01 14:53:00 +0530

    Make ValidateCell work in GameController

--- Skidadle-backend/src/main/java/com/example/skidadlebackend/controller/GameController.java
+++ Skidadle-backend/src/main/java/com/example/skidadlebackend/controller/GameController.java
@@ -1,12 +1,16 @@
 package com.example.skidadlebackend.controller;
 
 import com.example.skidadlebackend.model.GameResponse;
+import com.example.skidadlebackend.model.GameState;
 import com.example.skidadlebackend.model.PlaceTileRequest;
+import com.example.skidadlebackend.services.GameLogic;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/skidadle")
 public class GameController {
+    GameState gameState = new GameState(10, 10);
+
     @GetMapping("/welcome")
     public String hello() {
         final String GAME_NAME = "Skidadle";
@@ -20,6 +24,15 @@ public class GameController {
         int row = placeTileRequest.getRow();
         int col = placeTileRequest.getCol();
         char ch = placeTileRequest.getCh();
+
+        boolean isValid = GameLogic.validateCell(gameState, row, col, ch);
+
+        if (!isValid) {
+            return GameResponse.builder()
+                    .status("Failure")
+                    .error("Invalid cell")
+                    .build();
+        }
 
         return GameResponse.builder()
                 .status("Success")
