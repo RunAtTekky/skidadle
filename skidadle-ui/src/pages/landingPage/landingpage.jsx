cbdc83b1bbb411ec86840639c594dfbc931c7cd8
suryapratapsingh26 <146753809+suryapratapsingh26@users.noreply.github.com>
2026-07-01 18:02:13 +0530

    Add basic landing page skeleton

---
+++ skidadle-ui/src/pages/landingPage/landingpage.jsx
@@ -0,0 +1,15 @@
+import "./landingpage.css";
+
+function LandingPage() {
+  return (
+    <div className="container">
+      <h1 className="title">Skidadle</h1>
+
+      <div className="setup-card">
+        <h2 className="card-title">Game Setup</h2>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
