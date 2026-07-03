da77aefa2de0524e48c4336e350e8f285b92cf36
suryapratapsingh26 <146753809+suryapratapsingh26@users.noreply.github.com>
2026-07-04 00:41:00 +0530

    Update Button and TextInput components

--- skidadle-ui/src/components/Button/Button.jsx
+++ skidadle-ui/src/components/Button/Button.jsx
@@ -1,5 +1,11 @@
 import "./Button.css";
-function Button({ children }) {
-  return <button className="btn">{children}</button>;
+
+function Button({ children, onClick }) {
+  return (
+    <button className="btn" onClick={onClick}>
+      {children}
+    </button>
+  );
 }
+
 export default Button;
