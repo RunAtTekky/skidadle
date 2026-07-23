819827bbcc723597175c87cdd185797a048f3bea
RunAtTekky <varunrawat343@gmail.com>
2026-07-23 18:27:46 +0530

    Add: CustomErrorController and "/" mapping

--- Skidadle-backend/src/main/java/com/example/skidadlebackend/controller/HelloController.java
+++ Skidadle-backend/src/main/java/com/example/skidadlebackend/controller/HelloController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @GetMapping("/")
+    public String Home() {
+        return "Welcome to Skidadle";
+    }
+
     @GetMapping("/hello")
     public String Hello() {
         return "Hello World";
